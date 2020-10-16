cc0df37f5e2fbf7cc72de835ae2034bd33b5d865
dseng13 <dseng13@gmail.com>
2020-10-16 11:10:36 -0400

    moved dynamic script to separate file so it can run before page loads

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
-const metaDesc = document.getElementById('meta-description');
-
-const release = '12 Nov 2020';
-
 function countdown() {
     const releaseDate = new Date(release);
     const currentDate = new Date();
@@ -22,7 +18,6 @@ function countdown() {
     hoursEl.innerHTML = formatTime(hours);
     minutesEl.innerHTML = formatTime(minutes);
     secondsEl.innerHTML = formatTime(seconds);
-    metaDesc.innerHTML = `The future of gaming begins in ${days} days. Are you ready?`;
 }
 
 function formatTime(time) {
