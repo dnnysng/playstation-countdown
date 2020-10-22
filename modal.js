c5cbd0ee2c77ca3bb9a0b2139d7a731241e42652
dseng13 <dseng13@gmail.com>
2020-10-22 07:26:27 -0400

    changed to css modal, added triangle menu

--- modal.js
+++ modal.js
@@ -1,25 +1,25 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var squareModal = document.getElementById("squareModal");
 
 // Get the button that opens the modal
-var a = document.getElementById("myBtn");
+var squareBtn = document.getElementById("squareBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-a.onclick = function() {
-  modal.style.display = "block";
+squareBtn.onclick = function() {
+  squareModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  squareModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == squareModal) {
+    squareModal.style.display = "none";
   }
 }
